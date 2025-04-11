87cc7429af33d6dc01e54bff057c074d3966e1f5
rksumme <rebekah@wellwaterproductions.com>
2025-04-10 20:38:21 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/home.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/home.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
